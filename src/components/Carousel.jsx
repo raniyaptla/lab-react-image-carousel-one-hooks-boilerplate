22ff5b9ce9ce3361ae62b468a533f2d8033fc3a4
raniyaptla <ra.niyapoonthala@gmail.com>
2024-01-31 15:09:01 +0530

    upt

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,45 @@
+
+
 import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
-function Carousel() {
-}
+const Carousel = () => {
+  const [currImg, setCurrImg] = useState(0);
+
+  const handlePrevClick = () => {
+    if (currImg > 0) {
+      setCurrImg(currImg - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (currImg < images.length - 1) {
+      setCurrImg(currImg + 1);
+    }
+  };
+
+  return (
+    <div className="carousel">
+      <div
+        className="carouselInner"
+        style={{ backgroundImage: `url(${images[currImg].img})` }}
+      >
+        <div className="left" onClick={handlePrevClick}>
+          <ArrowBackIosIcon style={{ fontSize: 50 }} />
+        </div>
+        <div className="center">
+          <h1>{images[currImg].title}</h1>
+          <p>{images[currImg].subtitle}</p>
+        </div>
+        <div className="right" onClick={handleNextClick}>
+          <ArrowForwardIosIcon style={{ fontSize: 50 }} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
